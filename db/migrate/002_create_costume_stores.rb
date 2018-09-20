5697c89055916053180a6f2509108d20bd7b7f7e
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-20 23:45:58 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -13,11 +13,3 @@ class CreateCostumeStores < ActiveRecord::Migration[4.2]
     end
   end
 end
-
-# name
-# location
-# number of costumes, or "costume inventory"
-# number of employees
-# whether or not it's still in business
-# opening time
-# closing time
