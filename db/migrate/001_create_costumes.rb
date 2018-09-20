780a5a3d9dac8f5867ff96fd2c6e9cb3e1a46e13
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-20 23:35:45 +0000

    automatically backed up by learn

--- db/migrate/001_create_costumes.rb
+++ db/migrate/001_create_costumes.rb
@@ -13,6 +13,8 @@ class CreateCostumes <ActiveRecord::Migration[4.2]
     create_table :costumes do |t|
       t.string :name
       t.float :price
+      t.string :image_url
+      t.string :size
     end
   end
 end
