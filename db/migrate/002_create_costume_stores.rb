ac395c052e7764ff22d0f6480f124d9ea1393213
Allyson Hotchkin <ahotchk1@yahoo.com>
2018-09-20 23:40:38 +0000

    automatically backed up by learn

--- db/migrate/002_create_costume_stores.rb
+++ db/migrate/002_create_costume_stores.rb
@@ -1,1 +1,9 @@
 # Create your costume_stores migration here
+
+# name
+# location
+# number of costumes, or "costume inventory"
+# number of employees
+# whether or not it's still in business
+# opening time
+# closing time
